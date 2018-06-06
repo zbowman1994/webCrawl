51b157de4183c09ec6edd3c1ea7c3da77302ed39
Zack B <Zack B>
2018-06-05 19:33:01 -0700

    Cleaned up error handling

--- webCrawl.py
+++ webCrawl.py
@@ -3,27 +3,26 @@ import sys
 from bs4 import BeautifulSoup
 
 # Crawls a webpage and returns the number of instances of a given word
- 
-def main(argv):
-    url = "https://en.wikipedia.org/wiki/Oktoberfest"
-    if len(sys.argv) == 1:
-        print("No word entered in commandline")
-        return
-    elif len(sys.argv) > 2:
-        print("Too many words entered, just one word please")
-        return
-    else:
-        word = argv[1]
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html, "lxml")
 
-    for script in soup(["script", "style"]): # Get rid of all script and style elements
-        script.extract() 
-    
-    text = soup.get_text() # Get text from webpage
-    count = text.count(word)# Get count of requested word
-    print('\nUrl: {}\nContains {} occurrence(s) of word: {}'.format(url, count, word))
+def main():
+	while True:
+		try:
+			url = input("Enter a URL (exactly as-is in url bar) or type 'q' to quit: ")
+			if url == 'q':
+				print("Goodbye!")
+				return False	
+			word = input("Enter word to crawl site for: ")
+			html = urllib.request.urlopen(url).read()
+			soup = BeautifulSoup(html, "lxml")
+			for script in soup(["script", "style"]): # Get rid of all script and style elements
+				script.extract() 		
+			text = soup.get_text() # Get text from webpage
+			count = text.count(word)# Get count of requested word
+			print('\nUrl: {}\nContains {} occurrence(s) of word: {}'.format(url, count, word))
+			print("\n")
+		except ValueError:
+			print("\nInvalid URL, please try again\n")
  
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
 
